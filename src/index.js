d4d5b076689f84fc88c35edb8817ea053e8ab4ab
javyer <70788090+javyer12@users.noreply.github.com>
2022-05-09 21:11:07 -0600

    add random images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+/* <div class=" p-4">
+    <img 
+        class = "mx-auto"
+        width= "300"
+        src= "https://randomfox.ca/images/${nombre_image}"
+        alt="fox"
+    />
+</div>
+*/
+const max = 122;
+const min = 1;
 
-console.log('Happy hacking :)')
+const randomer = () => Math.floor(Math.random() * (max - min )) + min;
+
+const createImageNode  = () => {
+    const container = document.createElement("div");
+    container.className = "p-4";
+
+    const image = document.createElement("img");
+    image.className = "mx-auto";
+    image.width = "300";
+    image.height = "200";
+    image.src = `https://randomfox.ca/images/${randomer()}.jpg`;
+
+    container.appendChild(image);
+
+    return container;
+
+}
+const newImage = createImageNode();
+const mountNode = document.getElementById("images");
+
+const AddImage = document.getElementById("addImage");
+
+const addNewImage = () => {
+    const newFox = createImageNode();
+    mountNode.appendChild(newFox);
+
+}
+
+AddImage.addEventListener("click",addNewImage );
